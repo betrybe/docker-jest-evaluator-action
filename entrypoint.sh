c70fc256dd14725dad7dade9f8d292916333df2e
Rodrigo G. Oliveira <mjgargani@gmail.com>
2021-10-21 17:21:02 -0300

    put docker dind in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,33 @@ set -x
 run_npm_start=$1
 wait_for_url=$2
 
+docker_dir=$(pwd)/docker
+work_dir=/$EVAL_CONTAINER_NAME
+
+ls -la $(pwd)/docker
+
 docker rm -fv $EVAL_CONTAINER_NAME &> /dev/null
 
-npm install
+docker run \
+  --name $EVAL_CONTAINER_NAME \
+  --privileged \
+  -d \
+  -w $work_dir \
+  -v $docker_dir:$work_dir \
+  mjgargani/docker:dind-trybe1.0 \
 
-if $run_npm_start ; then
-  npm start & npx wait-on $wait_for_url
-fi
+docker exec $EVAL_CONTAINER_NAME ls -la
 
-npm test -- --json --outputFile=evaluation.json
-node /evaluator.js evaluation.json .trybe/requirements.json result.json
+docker exec $EVAL_CONTAINER_NAME ls -la ..
+
+# npm install
+
+# if $run_npm_start ; then
+#   npm start & npx wait-on $wait_for_url
+# fi
+
+# npm test -- --json --outputFile=evaluation.json
+# node /evaluator.js evaluation.json .trybe/requirements.json result.json
 
 docker rm -fv $EVAL_CONTAINER_NAME &> /dev/null
 
