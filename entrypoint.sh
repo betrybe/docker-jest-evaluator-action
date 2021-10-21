a52085e00041dacfcd0c258ea92f1772d8bab73a
Rodrigo G. Oliveira <mjgargani@gmail.com>
2021-10-21 17:51:00 -0300

    restore default

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,35 +4,35 @@ set -x
 run_npm_start=$1
 wait_for_url=$2
 
-docker_dir=$(pwd)/docker
-work_dir=/$EVAL_CONTAINER_NAME
+# docker_dir=$(pwd)/docker
+# work_dir=/$EVAL_CONTAINER_NAME
 
-ls -la $(pwd)/docker
+# ls -la $(pwd)/docker
 
-docker rm -fv $EVAL_CONTAINER_NAME &> /dev/null
+# docker rm -fv $EVAL_CONTAINER_NAME &> /dev/null
 
-docker run \
-  --name $EVAL_CONTAINER_NAME \
-  --privileged \
-  -d \
-  -w $work_dir \
-  -v $docker_dir:$work_dir \
-  mjgargani/docker:dind-trybe1.0 \
+# docker run \
+#   --name $EVAL_CONTAINER_NAME \
+#   --privileged \
+#   -d \
+#   -w $work_dir \
+#   -v $docker_dir:$work_dir \
+#   mjgargani/docker:dind-trybe1.0 \
 
-docker exec $EVAL_CONTAINER_NAME ls -la
+# docker exec $EVAL_CONTAINER_NAME ls -la
 
-docker exec $EVAL_CONTAINER_NAME ls -la ..
+# docker exec $EVAL_CONTAINER_NAME ls -la ..
 
-# npm install
+npm install
 
-# if $run_npm_start ; then
-#   npm start & npx wait-on $wait_for_url
-# fi
+if $run_npm_start ; then
+  npm start & npx wait-on $wait_for_url
+fi
 
-# npm test -- --json --outputFile=evaluation.json
-# node /evaluator.js evaluation.json .trybe/requirements.json result.json
+npm test -- --json --outputFile=evaluation.json
+node /evaluator.js evaluation.json .trybe/requirements.json result.json
 
-docker rm -fv $EVAL_CONTAINER_NAME &> /dev/null
+# docker rm -fv $EVAL_CONTAINER_NAME &> /dev/null
 
 if [ $? != 0 ]; then
   echo "Execution error"
