bf071ad166173cac37bd90e11d64af042519314b
Rodrigo G. Oliveira <mjgargani@gmail.com>
2021-10-25 19:49:30 -0300

    add export inline on run entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,19 @@
 #!/bin/sh -l
 set -x
 
-printenv
+export EVAL_CONTAINER_NAME="trybe-eval-$(cat /proc/sys/kernel/random/uuid)"
 
-# export EVAL_CONTAINER_NAME="trybe-eval-$(cat /proc/sys/kernel/random/uuid)"
+npm install
 
-# npm install
+npm test -- --json --outputFile=evaluation.json
 
-# npm test -- --json --outputFile=evaluation.json
+node ./.github/actions/docker-jest-evaluator/evaluator.js evaluation.json .trybe/requirements.json result.json
 
-# node ./.github/actions/docker-jest-evaluator/evaluator.js evaluation.json .trybe/requirements.json result.json
+docker rm -fv $EVAL_CONTAINER_NAME &> /dev/null
 
-# docker rm -fv $EVAL_CONTAINER_NAME &> /dev/null
+if [ $? != 0 ]; then
+  echo "Execution error"
+  exit 1
+fi
 
-# if [ $? != 0 ]; then
-#   echo "Execution error"
-#   exit 1
-# fi
-
-# echo ::set-output name=result::`cat result.json | base64 -w 0`
+echo ::set-output name=result::`cat result.json | base64 -w 0`
