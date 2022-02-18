5a8654033b76e546ee94566534888a6c4bc7df5f
Rodrigo G. Oliveira <mjgargani@gmail.com>
2022-02-18 11:48:11 -0300

    adc compose instalation

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,11 @@ fi
 npm install
 
 if [ "$up_compose" == "true" ]; then
+  # Install docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
   # Start student compose
   (cd $docker_challenges_folder && docker-compose up -d --build)
 
