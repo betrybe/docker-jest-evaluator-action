fcbc265f1c31f05a960f827b566c407a74046369
Rodrigo G. Oliveira <mjgargani@gmail.com>
2022-02-18 10:58:11 -0300

    adc puppeteer

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,15 @@
 set -x
 
 compose_challenges_folder=$INPUT_CHALLENGES_FOLDER
+requires_puppeteer_dependencies=$INPUT_PUPPETEER_TEST
 wait_for_url=$INPUT_WAIT_FOR
+
+if [ "$requires_puppeteer_dependencies" == "true" ]; then
+  sudo apt update
+
+  # Install chrome´s puppeteer requirements: https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#chrome-headless-doesnt-launch-on-unix
+  sudo apt install -y ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 lsb-release wget xdg-utils
+fi
 
 # Running the install before compose, as there may be post-install features
 npm install
@@ -20,6 +28,7 @@ if [ ! -z "$wait_for_url" ] ; then
   npx wait-on -t 300000 $wait_for_url # wait for server until timeout
 fi
 
+# Run jest test
 npm test -- --json --forceExit --outputFile=evaluation.json
 node /evaluator.js evaluation.json .trybe/requirements.json result.json
 
